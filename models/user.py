4992b466b4af7fafa0a4e61733d77b9c4ddf5384
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-10 19:02:13 +0100

    updated task 7

--- models/user.py
+++ models/user.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
 import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
@@ -11,13 +12,23 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(
         String(128), nullable=False
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     password = Column(
         String(128), nullable=False
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     first_name = Column(
         String(128), nullable=True
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     last_name = Column(
         String(128), nullable=True
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        'Place',
+        cascade="all, delete, delete-orphan",
+        backref='user'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+    reviews = relationship(
+        'Review',
+        cascade="all, delete, delete-orphan",
+        backref='user'
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
