dec613220304baa2c44bdd49d890c8c356436edb
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-10 17:59:49 +0100

    updated task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage
+or DBStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+import os
 
 
-storage = FileStorage()
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+
 storage.reload()
---
+++ models/engine/db_storage.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Definition of a class to manage database storage for AirBnb clone"""
+import os
+
