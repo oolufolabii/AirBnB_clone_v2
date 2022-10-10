8e7b1de8dc9c4fd3bb50eda0c3e90bd187c4280f
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-10 19:46:40 +0100

    updated task 7

--- models/__init__.py
+++ models/__init__.py
@@ -5,10 +5,11 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 import os
 
+storage = DBStorage() if os.getenv(
+    'HBNB_TYPE_STORAGE') == 'db' else FileStorage()
+"""A unique FileStorage/DBStorage instance for all models.
+"""
+storage.reload()
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    storage = DBStorage()
-else:
-    storage = FileStorage()
 
 storage.reload()
