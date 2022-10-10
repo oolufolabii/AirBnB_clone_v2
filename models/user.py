20688937a253ba621a3e49161266ed9368f2f78b
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-10 18:45:31 +0100

    updated task 7

--- models/user.py
+++ models/user.py
@@ -11,13 +11,13 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(
         String(128), nullable=False
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     password = Column(
         String(128), nullable=False
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     first_name = Column(
         String(128), nullable=True
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
     last_name = Column(
         String(128), nullable=True
-    )
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
