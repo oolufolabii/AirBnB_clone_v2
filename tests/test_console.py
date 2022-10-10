d847247ae6e156c138468d767e2e14446a26ec06
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-10 08:50:15 +0100

    updated

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,52 @@
 #!/usr/bin/python3
+"""Console Unittest"""
+
+import unittest
+from io import StringIO
+from unittest.mock import patch
+import pep8
+import os
+from os import getenv
+import console
+from console import HBNBCommand
+from models.engine.file_storage import FileStorage
+
+
+class TestConsole(unittest.TestCase):
+    """The console test class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Class setup for testing"""
+        cls.console = HBNBCommand()
+
+    def setUp(self):
+        """Setting up test cases"""
+        if os.path.isfile('file.json'):
+            os.remove('file.json')
+        self.resetStorage()
+
+    def resetStorage(self):
+        """Resetting the file storage"""
+        FileStorage._FileStorage__objects = {}
+
+        if os.path.isfile(FileStorage._FileStorage__objects):
+            os.remove(FileStorage._FileStorage__objects)
+
+    def test_pep8_console(self):
+        """Pep8 test for console"""
+        style = pep8.StyleGuide(quiet=True)
+        checker = style.check_files(["console.py"])
+        self.assertEqual(checker.total_errors, 0, 'fix Pep8')
+
+    def test_docstrings_in_console(self):
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_quit(self):
+        pass
+
+    def test_emptyline(self):
+        pass
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -2,6 +2,8 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+from unittest import getenv
+import unittest
 
 
 class test_User(test_basemodel):
@@ -32,3 +34,32 @@ class test_User(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.password), str)
+
+    def test_is_subclass_User(self):
+        """test if User is subclass of Basemodel"""
+        self.assertTrue(issubclass(self.user.__class__, BaseModel), True)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db",
+                     "can't run if storage is db")
+    def test_save_User(self):
+        """test if the save works"""
+        self.user.save()
+        self.assertNotEqual(self.user.created_at, self.user.updated_at)
+
+    def test_to_dict_User(self):
+        """test if dictionary works"""
+        self.assertEqual('to_dict' in dir(self.user), True)
+
+    def test_pep8_User(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/user.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_checking_for_docstring_User(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(User.__doc__)
+
+
+if __name__ == "__main__":
+    test_basemodel.main()
