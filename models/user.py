05d22a234739e4a04f38e7dd87a5100d384f863d
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-10 18:41:17 +0100

    updated task 7

--- models/user.py
+++ models/user.py
@@ -6,18 +6,18 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(
         String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    )
     password = Column(
         String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    )
     first_name = Column(
         String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    )
     last_name = Column(
         String(128), nullable=True
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    )
