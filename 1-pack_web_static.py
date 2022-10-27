2046a763bcc2d84c0fdb456b349f3c4e6bc3e191
oolufolabii <otitoola.olufolabi@gmail.com>
2022-10-27 08:38:10 +0100

    Web Static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive from the contents of
+the web_static folder of your AirBnB Clone repo, using the function
+do_pack"""
+
+from fabric.operations import local
+from datetime import datetime
+
+
+
+def do_pack():
+    """archiving all the static files"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers, using
+the function do_deploy"""
+
+import os
+from datetime import datetime
+from fabric.api import env, local, put, run, runs_once
+
+env.user = "ubuntu"
+"""user name"""
+
+env.hosts = ['18.232.125.137', '100.25.164.73']
+"""The host servers IP addresses."""
+
+@runs_once
+def do_pack():
+    """Function to compress files in an archive"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
+
+
+def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
+    """
+    if not os.path.exists(archive_path):
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
