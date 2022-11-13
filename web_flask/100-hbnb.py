11757e16677688cb65030d2ab00cd5f71740fdd2
oolufolabii <otitoola.olufolabi@gmail.com>
2022-11-13 15:28:03 +0100

    Web Framework

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Starts a web application using the Flask framework"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 
 from models import storage
 from models.amenity import Amenity
@@ -11,10 +11,25 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-
 @app.route('/hbnb')
-
-
+def hbnb():
+    '''The hbnb page.'''
+    all_states = list(storage.all(State).values())
+    amenities = list(storage.all(Amenity).values())
+    places = list(storage.all(Place).values())
+    all_states.sort(key=lambda x: x.name)
+    amenities.sort(key=lambda x: x.name)
+    places.sort(key=lambda x: x.name)
+    for state in all_states:
+        state.cities.sort(key=lambda x: x.name)
+    for place in places:
+        place.description = Markup(place.description)
+    content = {
+        'states': all_states,
+        'amenities': amenities,
+        'places': places
+    }
+    return render_template('100-hbnb.html', **content)
 
 
 @app.teardown_appcontext
